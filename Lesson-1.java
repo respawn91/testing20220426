004e120842268551480f89f0665c42c547ab048a
Anton Tarasov <respawn91@gmail.com>
2022-04-27 18:31:09 +0300

    Add files via upload

---
+++ Lesson-1.java
@@ -0,0 +1,138 @@
+// Инициализация переменных в Java
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        byte x = 10;
+        
+        System.out.println(x);
+        
+        x = 20;
+        
+        System.out.println(x);
+    }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        double x = 10;
+        
+        System.out.println(x);
+        
+        x = 20.734;
+        
+        System.out.println(x);
+    }
+}
+
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        String x = "Some text";
+        
+        System.out.println(x);
+        
+        x = "Another text";
+        
+        System.out.println(x);
+    }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        boolean x = false;
+        
+        System.out.println(x);
+        
+        x = true;
+        
+        System.out.println(x);
+    }
+}
+
+// Преобразование типов
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        int x = 10;
+        double y = 20.0;
+        
+        double z = x + y;
+        
+        System.out.println(z);
+        
+    }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        double x = 100.124;
+        
+        int y = (int)x;
+        
+        System.out.println(y);
+    }
+}
+
+// Основные арифметические процедуры
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        int a = 100;
+        int b = 20;
+        int c = 70;
+        
+        int s1 = a + b + c;
+        System.out.println(s1);
+        
+        int s2 = a - b - c;
+        System.out.println(s2);
+        
+        int s3 = a * b;
+        System.out.println(s3);
+        
+        int s4 = a / c;
+        System.out.println(s4);
+        
+        int s5 = a % c;
+        System.out.println(s5);
+        
+        a++;
+        System.out.println(a);
+        
+        b--;
+        System.out.println(b);
+    }
+}
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        int a = 50;
+        int b = 15;
+        
+        System.out.println(a / b);
+        System.out.println(a % b);
+        
+        double x = 50;
+        double y = 15;
+        
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
+
